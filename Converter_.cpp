eef6ee2a084606b68418bf516948a16771fce47e
Arman <110356055+Killmillk@users.noreply.github.com>
2023-06-03 13:36:23 +0300

    update performance

--- Converter_.cpp
+++ Converter_.cpp
@@ -1,6 +1,14 @@
-﻿#include<iostream>
+/*пример:
+* Рассмотрим случай, когда number равно 21.Здесь number / 10 это 2, так что tens[number / 10] это "двадцать".
+* Далее number % 10 это 1, так что convert(number % 10) это "один".
+* Тогда конечная строка будет "двадцать один"
+* И так со всем числами
+*/
+
+#include<iostream>
 #include<vector>
 #include <string> 
+#include<array>
 using namespace std;
 std::string nameForNumber(long long number);
 std::string thousandEnding(int number);
@@ -9,15 +17,11 @@ std::string billionEnding(int number);
 std::string rubleEnding(int number);
 
 
-/*пример самый наглядный будет:
-* Рассмотрим случай, когда number равно 21.Здесь number / 10 это 2, так что tens[number / 10] это "двадцать".
-* Далее number % 10 это 1, так что convert(number % 10) это "один".
-* Тогда конечная строка будет "двадцать один"
-* И так со всем числами
-*/
 
 std::string rubleEnding(int number) {
+    //берем 1 число с конца 
     int lastDigit = number % 10;
+    //берем 2 число с конца
     int penultimateDigit = (number / 10) % 10;
 
     if (penultimateDigit == 1) {  // Для чисел 10-19 всегда "рублей"
@@ -71,7 +75,6 @@ std::string millionEnding(int number) {
 }
 
 std::string billionEnding(int number) {
-    //берем 2 последних числа
     int lastDigit = number % 10;
     int penultimateDigit = (number / 10) % 10;
 
@@ -91,39 +94,25 @@ std::string billionEnding(int number) {
 // склонения  доделать - "одна"  и "две"
 std::string nameForNumber(long long number) {
 
-    static const std::vector<std::string> ones = { "", " один", " два", " три", " четыре", " пять", " шесть", " семь", " восемь", " девять" };
-    static const std::vector<std::string> teens = { " десять", " одиннадцать", " двенадцать", " тринадцать", " четырнадцать", " пятнадцать", " шестнадцать", " семнадцать", " восемнадцать", " девятнадцать" };
-    static const std::vector<std::string> tens = { "", "", " двадцать", " тридцать", " сорок", " пятьдесят", " шестьдесят", " семьдесят", " восемьдесят", " девяносто" };
-    static const std::vector<std::string> hundredth = { ""," сто", " двести", " триста", " четыреста", " пятьсот", " шестьсот", " семьсот", " восемьсот", " девятьсот", };
+    static const std::array<string, 10> ones = { "", " один", " два", " три", " четыре", " пять", " шесть", " семь", " восемь", " девять" };
+    static const std::array<string, 10> teens = { " десять", " одиннадцать", " двенадцать", " тринадцать", " четырнадцать", " пятнадцать", " шестнадцать", " семнадцать", " восемнадцать", " девятнадцать" };
+    static const std::array<string, 10> tens = { "", "", " двадцать", " тридцать", " сорок", " пятьдесят", " шестьдесят", " семьдесят", " восемьдесят", " девяносто" };
+    static const std::array<string, 10> hundredth = { ""," сто", " двести", " триста", " четыреста", " пятьсот", " шестьсот", " семьсот", " восемьсот", " девятьсот", };
 
     //единицы
-    if (number < 10 ) {
-        return ones[number];
-    }
+    if (number < 10 ) return ones[number];
     //числительные
-    else if (number < 20) {
-        return teens[number - 10];
-    }
+    else if (number < 20)  return teens[number - 10];
     //десятки
-    else if (number < 100) {
-        return tens[number / 10] + nameForNumber(number % 10);
-    }
+    else if (number < 100) return tens[number / 10] + nameForNumber(number % 10);
     //сотни
-    else if (number < 1000) {
-        return hundredth[number / 100] + nameForNumber(number % 100);
-    }
+    else if (number < 1000) return hundredth[number / 100] + nameForNumber(number % 100);
     //тысячи
-    else if (number < 1000000) {
-        return nameForNumber(number / 1000) + thousandEnding(number / 1000) + nameForNumber(number % 1000);
-    }
+    else if (number < 1000000) return nameForNumber(number / 1000) + thousandEnding(number / 1000) + nameForNumber(number % 1000);
     //миллионы
-    else if (number < 1000000000) {
-        return nameForNumber(number / 1000000) + millionEnding (number/ 1000000) + nameForNumber(number % 1000000);
-    }
+    else if (number < 1000000000)  return nameForNumber(number / 1000000) + millionEnding (number/ 1000000) + nameForNumber(number % 1000000);
     //миллиарды 
-    else if (number < 1000000000000) {
-        return nameForNumber(number / 1000000000) + billionEnding(number/ 1000000000) + nameForNumber(number % 1000000000);
-    }
+    else if (number < 1000000000000) return nameForNumber(number / 1000000000) + billionEnding(number/ 1000000000) + nameForNumber(number % 1000000000);
     return "error";
 }
 
@@ -136,8 +125,7 @@ int main()
     {
         cout << "Enter a positive integer: ";
         cin >> input;
-        cout << "\n" <<  nameForNumber(input) + rubleEnding(input) << endl;
-        cout << "\n\n" << endl;
+        cout << "\n" << nameForNumber(input) + rubleEnding(input) << "\n\n";
     } while (input > 0);
     return 0;
 }
